983b25f74d79ea883a3e98667a3e89fcc134b57b
Bojan-Lukic-994 <boka.lukic.994@gmail.com>
2018-08-10 14:09:24 +0200

    Started and finished new section - files, writing and reading files.

---
+++ Eclipse - workspace/files/src/files/FileWriteRunner.java
@@ -0,0 +1,21 @@
+package files;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class FileWriteRunner {
+
+	public static void main(String[] args) throws IOException {
+
+		Path pathFileToWrite = Paths.get("./resources/file-write.txt");
+
+		List<String> list = List.of("try", "Boy", "system", "Dog");
+
+		Files.write(pathFileToWrite, list);
+
+	}
+
+}
