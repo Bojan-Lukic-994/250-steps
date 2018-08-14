5c8aefc1e4fe9c006e9d1132b4148371137b0b08
Bojan-Lukic-994 <boka.lukic.994@gmail.com>
2018-08-14 11:25:04 +0200

    Added new files.
    Course finished.

---
+++ Eclipse - workspace/java.tips/src/java/tips/BlocksRunner.java
@@ -0,0 +1,12 @@
+package java.tips;
+
+public class BlocksRunner {
+
+	public static void main(String[] args) {
+
+		if (5 > 2) {
+			System.out.println("5>2");
+		}
+	}
+
+}
---
+++ Eclipse - workspace/java.tips/src/java/tips/ImportsRunner.java
@@ -0,0 +1,21 @@
+package java.tips;
+
+import static java.util.Collections.sort;
+
+import java.util.ArrayList;
+
+public class ImportsRunner {
+
+	public static void main(String[] args) {
+
+		System.out.println("Imports");
+		System.out.print("Static imports");
+
+
+		sort(new ArrayList<String>());
+
+		// BigDecimal db = null;
+
+	}
+
+}
